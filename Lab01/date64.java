fb887828154e9019b15aa11bf4872dc84b03dee1
dang04pham <53365514+dang04pham@users.noreply.github.com>
2024-03-15 23:59:05 +0700

    Add files via upload

--- Lab01/date64.java
+++ Lab01/date64.java
@@ -105,17 +105,17 @@ class DATE{
 
     public void evaluateDate(){
         if(month == 1 || month == 3 || month == 5 || month == 7 ||  month == 8 || month == 10 || month == 12){
-            System.out.println("31");
+            System.out.println("number of days: 31");
         }
         else if(month == 4 || month == 6 || month == 9 || month == 11){
-            System.out.println("30");
+            System.out.println("number of days: 30");
         }
         else if(month ==2){
             if(checkLeapYear(year) == 1){
-                System.out.println("29");
+                System.out.println("number of days: 29");
             }
             else{
-                System.out.println("28");
+                System.out.println("number of days: 28");
             }
         }
     }
